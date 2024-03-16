8615566a84fdabbafffb1c3854a78aee697ecb08
mohamed saadhath <saathathm@gmail.com>
2024-03-16 23:49:40 +0530

    works

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
     <div className='navbar'>
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p> SHOPPERR</p>
+        <p> SHOPPER</p>
       </div>
       <ul className="nav-menu">
         <li onClick={() => { setMenu('shop') }}> <Link to={'/'} style={{ textDecoration: 'none' }}>Shop</Link> {menu === 'shop' ? <hr /> : <></>}</li>
